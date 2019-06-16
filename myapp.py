59cf2eabd3e947c25da8d38c33bfc7a150a9cf56
Alexandros <43336997+Alexandros22@users.noreply.github.com>
2019-06-15 22:39:18 -0400

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -15,8 +15,4 @@ def salvador():
     return "Hello, Alexandros!"
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     app.run(host='127.0.0.1', port=5050)
-=======
-    app.run(host='127.0.0.1', port=5050)
->>>>>>> origin/master
